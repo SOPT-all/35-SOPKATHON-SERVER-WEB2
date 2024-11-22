f69476c1ba3ab7a3e1e8b80ffc36ddaa27133db2
Parkjyun <jaeyun0507@naver.com>
2024-11-23 03:45:41 +0900

    fix: controller 수정

--- src/main/java/com/sopkaton/web2/controller/HealthCheckController.java
+++ src/main/java/com/sopkaton/web2/controller/HealthCheckController.java
@@ -1,11 +1,14 @@
 package com.sopkaton.web2.controller;
 
 import com.sopkaton.web2.common.SuccessCode;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/health")
+@RestController
 public class HealthCheckController {
-        @RequestMapping("/check")
+        @GetMapping("/check")
         public ApiResponse<String> check() {
             return ApiResponse.success(SuccessCode.OK, "OK");
         }
