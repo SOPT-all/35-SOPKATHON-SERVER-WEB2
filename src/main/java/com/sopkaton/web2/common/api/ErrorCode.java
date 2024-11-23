ae09a9456c93c1d657b5dfce06ef3705c87797ee
Parkjyun <jaeyun0507@naver.com>
2024-11-24 02:37:27 +0900

    feat: 개별 조회 api 생성

--- src/main/java/com/sopkaton/web2/common/api/ErrorCode.java
+++ src/main/java/com/sopkaton/web2/common/api/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
     MISSION_NOT_FOUND(40402),
     USER_TEAM_NOT_FOUND(40403),
     TEAM_NOT_FOUND(40400),
-    INTERNAL_SERVER_ERROR(50000);
+    INTERNAL_SERVER_ERROR(50000),
+    NO_CHANCE(400000), CARD_NOT_FOUND(40404);
 
     private final int customErrorCode;
 }
---
+++ src/main/java/com/sopkaton/web2/service/response/CardResponse.java
@@ -0,0 +1,5 @@
+package com.sopkaton.web2.service.response;
+
+public record CardResponse(String hint, String phoneNumber, String mission) {
+
+}
