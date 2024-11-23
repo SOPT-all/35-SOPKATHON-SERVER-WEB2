920c9e6bcd78feb3b13f92d47bfd7b67ef95b361
Parkjyun <jaeyun0507@naver.com>
2024-11-23 23:50:38 +0900

    feat: 유저팀 영속성 엔티티 생성

---
+++ src/main/java/com/sopkaton/web2/repository/UserTeam.java
@@ -0,0 +1,36 @@
+package com.sopkaton.web2.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "user_team")
+public class UserTeam extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "user_team_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @Column(name = "chance", nullable = false)
+    private Boolean chance;
+}
