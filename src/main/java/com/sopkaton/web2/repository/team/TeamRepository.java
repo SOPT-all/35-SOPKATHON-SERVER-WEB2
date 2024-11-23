abb5f4948ad47afd16a55a7a0cc69fa8c1c32741
Parkjyun <jaeyun0507@naver.com>
2024-11-24 01:49:02 +0900

    feat: 팀 코어 관련 생성

--- src/main/java/com/sopkaton/web2/repository/team/TeamRepository.java
+++ src/main/java/com/sopkaton/web2/repository/team/TeamRepository.java
@@ -1,6 +1,10 @@
 package com.sopkaton.web2.repository.team;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findAllByNameIgnoreCaseContainingOrderByName(String name);
 }
--- src/main/java/com/sopkaton/web2/repository/team/TeamRetriever.java
+++ src/main/java/com/sopkaton/web2/repository/team/TeamRetriever.java
@@ -1,6 +1,9 @@
 package com.sopkaton.web2.repository.team;
 
+import com.sopkaton.web2.common.api.CustomException;
+import com.sopkaton.web2.common.api.ErrorCode;
 import com.sopkaton.web2.common.core.TeamNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +14,11 @@ public class TeamRetriever {
 
 
     public Team findTeamById(long randomTeamId) {
-        return teamRepository.findById(randomTeamId).orElseThrow(TeamNotFoundException::new);
+        return teamRepository.findById(randomTeamId).orElseThrow(() -> new CustomException(ErrorCode.TEAM_NOT_FOUND));
+    }
+
+    public List<Team> findTeamsByName(String name) {
+        return teamRepository.findAllByNameIgnoreCaseContainingOrderByName(name);
     }
 }
+
