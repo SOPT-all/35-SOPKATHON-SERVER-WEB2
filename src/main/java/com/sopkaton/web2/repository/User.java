bf4176c29f15cb63af090b538f32365f62add04b
Parkjyun <jaeyun0507@naver.com>
2024-11-23 23:50:28 +0900

    feat: 유저 영속성 엔티티 생성

---
+++ src/main/java/com/sopkaton/web2/repository/User.java
@@ -0,0 +1,28 @@
+package com.sopkaton.web2.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
