2544b9c650299ce8709a30f0010f9fd54dc48b9f
Parkjyun <jaeyun0507@naver.com>
2024-11-23 23:50:33 +0900

    feat: 팀 영속성 엔티티 생성

---
+++ src/main/java/com/sopkaton/web2/repository/Team.java
@@ -0,0 +1,31 @@
+package com.sopkaton.web2.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "team")
+public class Team extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "team_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Column(name = "minimum_start_number", nullable = false)
+    private int minimumStartNumber;
+}
